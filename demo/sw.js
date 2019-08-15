c3e75b15a172d338c5f73e579c36b990d02b7d0d
Kirill Chugainov <chugainov.kirill@gmail.com>
2019-08-15 21:19:48 +0300

    Update sw.js

--- demo/sw.js
+++ demo/sw.js
@@ -25,10 +25,14 @@ function sendToChat()
 
     db.table('messages').each(function (item) {
 
-      fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${item.text}`, params);
+      
     });
 
     db.table('messages').clear();
+    
+    const text = 'Ай ай ай! Как нехорошо!'
+    
+    fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${text}`, params);
   });
 
   // const db = new Dexie("UnsyncData");
